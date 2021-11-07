3351ac533a8c11beff2e659ae1066f92dea60ae0
Alexandr Siryj <theobroma222@gmail.com>
2021-11-07 06:15:10 +0200

    roboto font

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import AppThemeProvider from "./@themes/theme";
 
+// Open Source fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
