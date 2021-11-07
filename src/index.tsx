3e4c0bdc7b2115218589e258f2e371c6bd7656d1
Alexandr Siryj <theobroma222@gmail.com>
2021-11-07 06:02:52 +0200

    add @mui & theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import AppThemeProvider from "./@themes/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AppThemeProvider>
+        <App />
+        {/* <AppContainer /> */}
+      </AppThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
